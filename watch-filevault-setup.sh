f7b8f0c8c23887528d6ac9f037d167e5b3d81d3f
Richard Bullington-McGuire <richard@bullington-mcguire.com>
2019-10-21 10:22:19 -0400

    Add script to watch filevault progress % on macOS

---
+++ watch-filevault-setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# watch-filevalult-setup.sh
+#
+# On macOS, watch the progress of FileVault and log the progress to a file.
+#
+# Copyright (C) 2019 by The Obscure Organization
+# MIT licensed. See the LICENSE file for details.
+
+# Set unofficial bash strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+LOGFILE="$HOME/fdesetup-status.txt"
+INTERVAL=1800 # 1800 seconds is 30 minutes
+
+watch -n "$INTERVAL" '
+    printf "Logging FileVault setup progress to '"$LOGFILE"'\n\n";
+    (
+        date -R
+        fdesetup status
+    )           |  
+    tr "\n" " " |
+    tee -a '"$LOGFILE"
