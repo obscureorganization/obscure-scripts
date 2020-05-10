a72790dea3aa58f92df061d78485940dc5464bb0
Richard Bullington-McGuire <richard@moduscreate.com>
2020-05-10 10:57:46 -0400

    Simplify and make shellcheck clean

--- imapd-reap-processes.sh
+++ imapd-reap-processes.sh
@@ -2,7 +2,7 @@
 #
 # imapd-reap-processes.sh
 #
-# Mail servers sometimes accumulate imapd processes that
+# Mail servers sometimes accumulate root-owned imapd processes that
 # started and never got killed off for some reason.
 # This script purges those, to save process slots and memory.
 #
@@ -19,6 +19,9 @@
 #
 # 1.0 (January 6, 2020)
 #  First public release
+#
+# 1.1 (May 10, 2020)
+#  Shellcheck fix and simplification
 
 # Set unofficial bash strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
@@ -30,14 +33,10 @@ DEBUG=${DEBUG:-false}
 $DEBUG && export PS4='${LINENO}: ' && set -x
 
 set +e
-PROCESSES=$(ps auxfw \
-    | grep ^root.*imapd \
-    | grep -v grep \
-    | grep -v \\_ \
-    | sed 's/  */ /g' \
-    | cut -f 2 -d' ')
+PROCESSES=$(pgrep -U 0 imapd)
 $DEBUG && echo "$0: processes: $PROCESSES"
 set -e
 if [[ -n "$PROCESSES" ]]; then
+    #shellcheck disable=SC2086
     kill $PROCESSES
 fi
